c2b34ff2be51c4cf2edb1cdd5422fc55d21774c1
Ahmed <bregsl2015@outlook.com>
2025-11-20 20:57:55 +0200

    ToCategory

---
+++ wwwroot/js/common.js
@@ -0,0 +1,9 @@
+﻿function ShowConfirmationModel()
+    {
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('bsConfirmationModal')).show();
+    }
+
+function HideConfirmationModel()
+{
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('bsConfirmationModal')).hide();
+    }
